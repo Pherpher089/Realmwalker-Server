e0e5f8edc7174d2bc8104e49c554a133bc0c16d3
Pherpher089 <christutor089@gmail.com>
2024-04-22 19:56:44 -0700

    adding rejectUnaruthorised

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,7 @@ module.exports = {
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
-
+    ssl: { rejectUnauthorized: false },
     migrations: {
       tableName: "knex_migrations",
       directory: "./data/migrations",
